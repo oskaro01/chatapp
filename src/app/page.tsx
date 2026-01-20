e04b31936ac880be5fcc3ba5970ab302df240ab4
oskaro01 <oskarode80@gmail.com>
2026-01-20 22:17:23 +0600

    persistent-unique-id-generation-in-local-storage-browser

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,36 @@
 "use client"
 
-import { useState } from "react";
+import { nanoid } from "nanoid";
+import { useEffect, useState } from "react";
+
+
+const ANIMALS = ["cat", "dog", "owl", "crow"]
+const STORAGE_KEY = "chat_username"
+
+const generateUsername = () => {
+  const word = ANIMALS[Math.floor(Math.random() * ANIMALS.length)]
+    return `anonymous-${word}-${nanoid(5)}`
+  
+}
 
 export default function Home() {
   const [username, setUsername] = useState("");
+
+  useEffect(() => {
+    const main =() => {
+      const stored = localStorage.getItem(STORAGE_KEY)
+
+      if (stored) {
+        setUsername(stored)
+        return
+      }
+
+      const generated = generateUsername()
+      localStorage.setItem(STORAGE_KEY, generated)
+    }
+
+    main()
+  }, [])
 
   return (
 
